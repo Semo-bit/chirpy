02d96976f76053f64f00946998389185bd2cb89f
Semo <salaminm@gmail.com>
2024-05-28 07:38:41 -0500

    Added handlers to count and reset number of visits

--- chirpy/main.go
+++ chirpy/main.go
@@ -1,24 +1,17 @@
 package main
+
 import (
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func main() {
-    const port = "8080"
-    mux := http.NewServeMux()
-    srv := &http.Server{
-        Addr: ":" + port,
-        Handler: mux,
-    }
-    mux.HandleFunc("/healthz", healtzHandler)
-
-    fileServer := http.FileServer(http.Dir("."))
-    mux.Handle("/app/", http.StripPrefix("/app", fileServer))
-    log.Printf("Serving on port: %s\n", port)
-    log.Fatal(srv.ListenAndServe())
+// Struct to hold the number of times our page was visted
+type apiConfig struct {
+    fileServerHits int
 }
 
+// Hanlder for health check
 func healtzHandler(w http.ResponseWriter, r *http.Request) {
     // Set the Content-Type header
     w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -30,3 +23,46 @@ func healtzHandler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("OK"))
     
 }
+
+// Function to count the number of hits
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileServerHits++
+        next.ServeHTTP(w, r)
+    })
+} 
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hits: %d", cfg.fileServerHits)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+    cfg.fileServerHits = 0
+    w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+    const port = "8080"
+    mux := http.NewServeMux()
+    srv := &http.Server{
+        Addr: ":" + port,
+        Handler: mux,
+    }
+    mux.HandleFunc("GET /healthz", healtzHandler)
+
+    // initialzing the apiConfig
+    apiCfg := &apiConfig{fileServerHits: 0}
+    
+    fileServer := http.FileServer(http.Dir("."))
+    handler := http.StripPrefix("/app", fileServer)
+    mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+
+    // creating a new handler to report hits
+    mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
+    // creating a new handler to reset hits
+    mux.HandleFunc("/reset", apiCfg.resetHandler)
+
+    log.Printf("Serving on port: %s\n", port)
+    log.Fatal(srv.ListenAndServe())
+}  
